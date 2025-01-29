83110c5009de6f6210253d137f4368e90caf63ee
Tamim0133 <tamimdewanandnobody@gmail.com>
2025-01-29 23:42:13 +0600

    Use Focus Cancelled

--- frontend/app/(tabs)/index.jsx
+++ frontend/app/(tabs)/index.jsx
@@ -1,4 +1,4 @@
-import React, { useCallback, useEffect, useMemo, useState } from 'react';
+import React, { useEffect, useMemo, useState } from 'react';
 import { Header, ListingsBottomSheet, ListingsMap, CustomDrawerContent } from '@/components';
 import { settings, AboutUsScreen, PrivacyPolicyScreen, RefundPolicyScreen, TermsAndConditionsScreen, ContactUsScreen, } from '@/app/otherScreens';
 import { createDrawerNavigator } from '@react-navigation/drawer';
@@ -6,7 +6,6 @@ import listingsDataGeo from '@/assets/data/airbnb-listings.geo.json';
 import { View, Alert } from 'react-native';
 import axios from 'axios';
 import * as Location from 'expo-location';
-import { useFocusEffect } from '@react-navigation/native';
 
 const Drawer = createDrawerNavigator();
 
@@ -24,45 +23,41 @@ const MainScreen = () => {
     const getoItems = useMemo(() => listingsDataGeo, []);
 
     const [items, setItems] = useState([]); // State for storing fetched listings
-    useFocusEffect(
-        useCallback(() => {
-            const fetchAds = async () => {
-                try {
-                    const response = await axios.get(`${baseURL}/api/ad/get-ad`);
-                    if (!response.data.success) {
-                        Alert.alert('Error', response.data.message);
-                        return;
-                    }
-                    setItems(response.data.data); // Update the listings state
-                } catch (error) {
-                    const errorMessage =
-                        error.response?.data?.message || 'An unexpected error occurred. Please try again.';
-                    Alert.alert('Error', errorMessage);
+
+    useEffect(() => {
+        const fetchAds = async () => {
+            try {
+                const response = await axios.get(`${baseURL}/api/ad/get-ad`);
+                if (!response.data.success) {
+                    Alert.alert('Error', response.data.message);
+                    return;
                 }
-            };
+                const newItems = response.data.data;
+                setItems(newItems); // Update the listings state
+            } catch (error) {
+                const errorMessage =
+                    error.response?.data?.message || 'An unexpected error occurred. Please try again.';
+                Alert.alert('Error', errorMessage);
+            }
+        };
 
-            const fetchLocation = async () => {
-                try {
-                    const { status } = await Location.requestForegroundPermissionsAsync();
-                    if (status !== 'granted') {
-                        console.warn("Location permission not granted");
-                        return;
-                    }
-                    const location = await Location.getCurrentPositionAsync({});
-                    setMyLocation(location.coords); // Update state with fetched location
-                } catch (err) {
-                    console.warn("Error getting location:", err);
+        const fetchLocation = async () => {
+            try {
+                const { status } = await Location.requestForegroundPermissionsAsync();
+                if (status !== 'granted') {
+                    console.warn("Location permission not granted");
+                    return;
                 }
-            };
+                const location = await Location.getCurrentPositionAsync({});
+                setMyLocation(location.coords); // Update state with fetched location
+            } catch (err) {
+                console.warn("Error getting location:", err);
+            }
+        };
 
-            fetchAds();
-            fetchLocation();
-
-            return () => {
-                // Cleanup if necessary
-            };
-        }, [])
-    );
+        fetchAds();
+        fetchLocation(); // Fetch location when the component mounts
+    }, []);
 
     return (
         <View style={{ flex: 1 }}>
